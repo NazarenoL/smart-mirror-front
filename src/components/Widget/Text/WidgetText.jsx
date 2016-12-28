6e285ea0df3386369660a651c1a06964c7eda284
Nazareno Lorenzo <nazareno@bunq.com>
2016-12-28 02:42:33 +0100

    Moved textSize to WidgetText instead of base Widget.

--- src/components/Widget/Text/WidgetText.jsx
+++ src/components/Widget/Text/WidgetText.jsx
@@ -2,11 +2,36 @@ import React, { Component } from 'react';
 import Widget from '../Widget.jsx';
 
 class WidgetText extends Component {
+  getTextClassName() {
+    let classes = [];
+
+    if(this.props.parameters && this.props.parameters.fontSize && this.props.parameters.fontSize.length > 0) {
+      classes.push(`font-size-${this.props.parameters.fontSize}`);
+    }
+
+    return classes.join(' ');
+  }
+
   render() {
+    if(this.props.parameters && this.props.parameters.text && this.props.parameters.text.length > 0) {
+      var text = (
+        <p className={this.getTextClassName()}>
+          {this.props.parameters.text}
+        </p>
+      );
+    }
+
+    if(this.props.parameters && this.props.parameters.title && this.props.parameters.title.length > 0) {
+      var title = (
+        <h1>{this.props.parameters.title}</h1>
+      );
+    }
+
     return (
       <div>
         <Widget module="Text" parameters={this.props.parameters}>
-          {this.props.parameters.text}
+          {title}
+          {text}
         </Widget>
       </div>
     )
--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -6,11 +6,6 @@ class Widget extends Component {
     //Determine base classes
     let classes = ['Widget', `Widget${this.props.module}`];
 
-    //Add font-size-xx class if present in parameters
-    if(this.props.parameters && this.props.parameters.fontSize && this.props.parameters.fontSize.length > 0) {
-      classes.push(`font-size-${this.props.parameters.fontSize}`);
-    }
-
     //Add extra classes if present in parameters
     if (this.props.extraClasses && this.props.extraClasses.length > 0) {
       classes.push(...this.props.extraClasses);
